8de806053e039df274f59129df859c4974d0b1ac
cainlucastapp <cainlucastapp@agentformula.com>
2026-05-15 13:10:16 -0700

    Created Password Endpoints

--- server/app.py
+++ server/app.py
@@ -6,6 +6,8 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User, UserSchema
 
+user_schema = UserSchema()
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -14,8 +16,58 @@ class ClearSession(Resource):
         session['user_id'] = None
 
         return {}, 204
+    
+class Signup(Resource):
+
+    def post(self):
+        data = request.get_json()
+
+        new_user = User(username=data['username'])
+        new_user.password_hash = data['password']  # triggers bcrypt setter
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['user_id'] = new_user.id  # log user in via session
+
+        return user_schema.dump(new_user), 201
+    
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            if user:
+                return user_schema.dump(user), 200
+
+        return {}, 204
+    
+class Login(Resource):
+
+    def post(self):
+        data = request.get_json()
+
+        user = User.query.filter_by(username=data['username']).first()
+
+        if user and user.authenticate(data['password']):
+            session['user_id'] = user.id
+            return user_schema.dump(user), 200
+
+        return {'error': 'Invalid username or password'}, 401
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
